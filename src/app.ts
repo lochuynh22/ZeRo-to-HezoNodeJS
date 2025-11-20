c93a5f6529a72939e0fd0a9d7b983cd4fdc17e22
lochuynh22 <huynhlatienloc123@gmail.com>
2025-11-21 01:15:14 +0700

    next

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World ss1!')
+})
+
+app.get('/demo', (req, res) => {
+  res.send('Demo1 2 ' )
 })
 
 app.listen(port, () => {

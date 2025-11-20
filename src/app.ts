25ba2ca8e556baab4ca93917141f030991fc71c1
lochuynh22 <huynhlatienloc123@gmail.com>
2025-11-21 02:59:51 +0700

    ss

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 // const express = require('express')
 import  express  from "express"
 import 'dotenv/config'
+import { dirname } from "path";
 const app = express()
 const port = process.env.PORT || 8000;
 
+//config view engine
+app.set('view engine', 'ejs')
+app.set("views",__dirname+"/views")
 app.get('/', (req, res) => {
-  res.send('Hello World ss1!')
+  res.render("home.ejs")
 
 })
 
 app.get('/demo', (req, res) => {
-  res.send('Demo1 2 ' )
+  res.send('Demo1 2  3 text ' )
 })
 
 app.listen(port, () => {

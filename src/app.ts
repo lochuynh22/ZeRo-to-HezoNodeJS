f3b5607d83c24d74a3f91fc953798046b4d05ce8
lochuynh22 <huynhlatienloc123@gmail.com>
2025-11-21 02:28:27 +0700

    4th

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 // const express = require('express')
 import  express  from "express"
+import 'dotenv/config'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello World ss1!')
+
 })
 
 app.get('/demo', (req, res) => {
@@ -13,4 +15,5 @@ app.get('/demo', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+    console.log("dot env:",process.env.PORT)
 })
